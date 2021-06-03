d112eb27deeb3bd560f42c3a00156828d0b16f97
Arnaud POINTET <arnaud.pointet@gmail.com>
2021-06-03 18:38:01 +0200

    Feature: Add language change

--- src/ReadAndSpeech.js
+++ src/ReadAndSpeech.js
@@ -2,10 +2,12 @@ import ReadAndSpeechNotSupported from './exceptions/ReadAndSpeechNotSupported'
 
 class ReadAndSpeech {
   constructor() {
+    console.log('it\'s my local')
     if (! ('speechSynthesis' in window)) {
       throw new ReadAndSpeechNotSupported('speechSynthesis is not supported on your browser')
     }
 
+    this.awaitVoices = new Promise(done => window.speechSynthesis.onvoiceschanged = done)
     this.synthesis = new SpeechSynthesisUtterance()
   }
 
@@ -13,6 +15,20 @@ class ReadAndSpeech {
     this.synthesis.text = msg
 
     return window.speechSynthesis.speak(this.synthesis)
+  }
+
+  async speakers() {
+    const voices = await this.awaitVoices
+
+    return window.speechSynthesis.getVoices()
+  }
+
+  changeVoice(voice) {
+    this.synthesis.voice = voice
+  }
+
+  currentVoice() {
+    return this.synthesis.voice
   }
 }
 
